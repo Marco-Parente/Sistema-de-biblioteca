6683b642fefd6cc2095fe0807c627fec2dfe6d90
paulacgates <49040288+paulacgates@users.noreply.github.com>
2019-04-07 21:15:10 -0400

    Add files via upload

---
+++ Funcoes/usuarios.py
@@ -0,0 +1,40 @@
+import os
+import csv 
+from .Auxiliares import*
+
+def CadastrarNovoUsuario():
+    os.system("clear")
+    print(" - Cadastrar Usuario - ")
+    print("")
+    Usuario = {}
+    print("O usuário é Discente ou Docente?")
+    resposta= input("-> ")
+    if resposta.upper() == "Discente":
+        Usuario.update({"Tipo": resposta})
+    elif resposta.upper( == "Docente")
+    else:
+        input('Resposta inválida. Pressione qualquer tecla...')
+        CadastrarNovoUsuario()
+    print("Digite seu CPF:")
+    CPF = input("-> ")
+    Usuario.update({"CPF": CPF})
+    print("Digite seu nome:")
+    Nome = input("-> ")
+    Usuario.update({"Nome": Nome})
+    Usuario.update({"QuantLivrosEmprestado": 0})
+    Usuario.update({"Data": ""})
+    arquivoExiste = os.path.isfile("livros.csv")
+
+    with open("usuarios.csv", "a", newline="") as arquivo:
+        escritor = csv.DictWriter(arquivo, fieldnames=camposUsuarios())
+        if not arquivoExiste:
+            escritor.writeheader()
+
+        escritor.writerow(Usuario)
+    arquivo.close()
+
+def AtualizarUsuario():
+
+
+def BuscarUsuario():
+
